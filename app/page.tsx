96931f5654e14fe5a6dc4e395f7d44e3abbdbf53
David Dossett <ddossett@microsoft.com>
2024-10-04 09:37:17 -0700

    Init commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,9 @@ import {
   Avatar,
   CounterLabel,
   Header,
+  Octicon,
 } from "@primer/react";
+import { MarkGithubIcon } from "@primer/octicons-react";
 import { useState, useEffect } from "react";
 
 const getRandomComponents = () => {
@@ -49,19 +51,7 @@ const NavItemContent = ({ index }: { index: number }) => {
   );
 };
 
-export default function Home() {
-  const [currentItem, setCurrentItem] = useState(0);
-
-  const navItems = Array.from({ length: 50 }, (_, index) => (
-    <NavList.Item
-      key={index}
-      aria-current={index === currentItem ? "page" : undefined}
-      onClick={() => setCurrentItem(index)}
-    >
-      Nav Item {index + 1}
-    </NavList.Item>
-  ));
-
+const HeaderComponent = () => {
   const headerLinkStyles = {
     color: "fg.default",
     "&:hover, &:active, &:visited": {
@@ -71,64 +61,134 @@ export default function Home() {
   };
 
   return (
+    <Header
+      sx={{
+        backgroundColor: "transparent",
+        borderBottom: "1px solid",
+        borderColor: "border.default",
+      }}
+    >
+      <Header.Item full>
+        <Header.Link href="#" fontSize={2} sx={headerLinkStyles}>
+          <Octicon
+            icon={MarkGithubIcon}
+            size={32}
+            sx={{
+              mr: 2,
+            }}
+          />
+          Header
+        </Header.Link>
+      </Header.Item>
+      <Header.Item sx={{ marginLeft: 3 }}>
+        <Header.Link href="#" fontSize={2} sx={headerLinkStyles}>
+          Link 1
+        </Header.Link>
+      </Header.Item>
+      <Header.Item sx={{ marginLeft: 3 }}>
+        <Header.Link href="#" fontSize={2} sx={headerLinkStyles}>
+          Link 2
+        </Header.Link>
+      </Header.Item>
+    </Header>
+  );
+};
+
+const NavigationComponent = ({
+  setCurrentItem,
+}: {
+  setCurrentItem: (index: number) => void;
+}) => {
+  const [currentItem, setCurrentItemState] = useState(0);
+
+  const handleItemClick = (index: number) => {
+    setCurrentItemState(index);
+    setCurrentItem(index);
+  };
+
+  const navItems = Array.from({ length: 50 }, (_, index) => (
+    <NavList.Item
+      key={index}
+      aria-current={index === currentItem ? "page" : undefined}
+      onClick={() => handleItemClick(index)}
+    >
+      Nav Item {index + 1}
+    </NavList.Item>
+  ));
+
+  const groupedNavItems = [];
+  for (let i = 0; i < navItems.length; i += 10) {
+    groupedNavItems.push(
+      <NavList.Group title="Section" key={i}>
+        {navItems.slice(i, i + 10)}
+      </NavList.Group>
+    );
+  }
+
+  return (
+    <Box
+      sx={{
+        maxWidth: "300px",
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: "300px",
+        borderRight: "1px solid",
+        borderColor: "border.default",
+        paddingX: 2,
+        overflowY: "auto",
+        height: "100%",
+      }}
+    >
+      <NavList>{groupedNavItems}</NavList>
+    </Box>
+  );
+};
+
+const ContentComponent = ({ currentItem }: { currentItem: number }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        overflowY: "auto",
+      }}
+    >
+      <NavItemContent index={currentItem} />
+    </Box>
+  );
+};
+
+export default function Home() {
+  const [currentItem, setCurrentItem] = useState(0);
+
+  return (
     <ThemeProvider colorMode="night">
       <BaseStyles>
-        <Header
-          sx={{
-            backgroundColor: "transparent",
-            borderBottom: "1px solid",
-            borderColor: "border.default",
-          }}
-        >
-          <Header.Item full>
-            <Header.Link href="#" fontSize={2} sx={headerLinkStyles}>
-              Header
-            </Header.Link>
-          </Header.Item>
-          <Header.Item sx={{ marginLeft: 3 }}>
-            <Header.Link href="#" fontSize={2} sx={headerLinkStyles}>
-              Link 1
-            </Header.Link>
-          </Header.Item>
-          <Header.Item sx={{ marginLeft: 3 }}>
-            <Header.Link href="#" fontSize={2} sx={headerLinkStyles}>
-              Link 2
-            </Header.Link>
-          </Header.Item>
-        </Header>
         <Box
           sx={{
             display: "flex",
-            flexDirection: "row",
-            maxWidth: "100%",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+            overflow: "hidden",
           }}
         >
-          <Box
-            sx={{
-              maxWidth: "300px",
-              flexGrow: 0,
-              flexShrink: 0,
-              flexBasis: "300px",
-              borderRight: "1px solid",
-              borderColor: "border.default",
-              paddingX: 2,
-              overflowY: "auto",
-              height: "100vh",
-            }}
-          >
-            <NavList>{navItems}</NavList>
-          </Box>
+          <HeaderComponent />
           <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
-              height: "100vh",
+              flexDirection: "row",
+              maxWidth: "100%",
               flexGrow: 1,
-              justifyContent: "center",
-              alignItems: "center",
+              overflow: "hidden",
             }}
           >
-            <NavItemContent index={currentItem} />
+            <NavigationComponent setCurrentItem={setCurrentItem} />
+            <ContentComponent currentItem={currentItem} />
           </Box>
         </Box>
       </BaseStyles>
