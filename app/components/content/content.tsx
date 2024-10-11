e27e492db906f72e9569efd8590c4364e07a39df
David Dossett <ddossett@microsoft.com>
2024-10-10 22:09:35 -0700

    Fix loading state

--- app/components/content/content.tsx
+++ app/components/content/content.tsx
@@ -15,41 +15,45 @@ export const Content: React.FC<ContentProps> = ({ issue, loading }) => {
   const { avatarUrls, avatarLoading } = useFetchAvatarUrl(issue);
   // const { issueSummaries, summaryLoading } = useFetchIssueSummary(issue);
 
-  if (!issue) {
-    return null;
-  }
-  const formattedDate = new Date(issue.created_at).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  const formattedDate = issue?.created_at
+    ? new Date(issue.created_at).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })
+    : "Unknown date";
 
-  const LoadingAuthorInfo = () => (
-    <>
-      <SkeletonText size={"titleMedium"} />
-      <Box className={styles.issueMeta}>
-        <SkeletonAvatar size={20} square={false} />
-        <SkeletonText size={"bodySmall"} maxWidth={150} />
-      </Box>
-    </>
-  );
+  const IssueAuthor = () => {
+    if (loading || !issue?.user) {
+      return (
+        <>
+          <Box className={styles.issueMeta}>
+            <SkeletonAvatar size={20} square={false} />
+            <SkeletonText size={"bodySmall"} maxWidth={150} />
+          </Box>
+        </>
+      );
+    }
 
-  const AuthorInfo = () => (
-    <>
-      <Box className={styles.issueMeta}>
-        {avatarLoading || !issue?.user ? (
-          <SkeletonAvatar size={20} square={false} />
-        ) : (
-          <Avatar src={avatarUrls[issue.user.login]} />
-        )}
-        <Text as="p" className={styles.issueUser}>
-          {issue?.user?.login ?? "Unknown"}
-        </Text>
-        <Text as="span" className={styles.issueCreatedAt}>
-          opened on {formattedDate}
-        </Text>
-      </Box>
-    </>
+    return (
+      <>
+        <Box className={styles.issueMeta}>
+          {avatarLoading ? <SkeletonAvatar size={20} square={false} /> : <Avatar src={avatarUrls[issue.user.login]} />}
+          <Text as="p" className={styles.issueUser}>
+            {issue.user.login}
+          </Text>
+          <Text as="span" className={styles.issueCreatedAt}>
+            opened on {formattedDate}
+          </Text>
+        </Box>
+      </>
+    );
+  };
+
+  const IssueTitle = () => (
+    <Text as="h2" className={styles.issueTitle}>
+      {loading ? <SkeletonText size={"titleMedium"} /> : issue.title}
+    </Text>
   );
 
   const IssueHeaderToolbar = () => (
@@ -60,20 +64,24 @@ export const Content: React.FC<ContentProps> = ({ issue, loading }) => {
     </ActionBar>
   );
 
+  const IssueBody = () => (
+    <Box className={styles.issueBody}>
+      {loading ? <SkeletonText lines={4} /> : <ReactMarkdown>{issue.body}</ReactMarkdown>}
+    </Box>
+  );
+
   return (
     <Box className={styles.container}>
       <Box className={styles.innerContainer}>
         <Box className={styles.issueHeader}>
-          <Text as="h2" className={styles.issueTitle}>
-            {issue.title}
-          </Text>
+          <IssueTitle />
           <Box className={styles.issueHeaderToolbar}>
-            {loading || !issue?.user ? <LoadingAuthorInfo /> : <AuthorInfo />}
+            <IssueAuthor />
             <IssueHeaderToolbar />
           </Box>
         </Box>
         <Box className={styles.mainContent}>
-          {loading ? <SkeletonText lines={4} /> : <ReactMarkdown>{issue.body}</ReactMarkdown>}
+          <IssueBody />
         </Box>
       </Box>
     </Box>
